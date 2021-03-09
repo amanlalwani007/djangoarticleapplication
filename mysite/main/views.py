3c675b62ca9af1cd552f6d022f970a452d3a952a
amanlalwani007 <amanlalwani0807@gmail.com>
2021-03-10 00:53:32 +0530

    completed website

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,60 +1,70 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Tutorial,TutorialCategory,TutorialSeries
+from .models import Tutorial, TutorialCategory, TutorialSeries
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login,logout,authenticate
-from django.contrib import messages 
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 from .forms import NewUserForm as UserCreationForm
 
-def single_slug(request,single_slug):
+
+def single_slug(request, single_slug):
     categories = [c.category_slug for c in TutorialCategory.objects.all()]
     if single_slug in categories:
-        matching_series = TutorialSeries.objects.filter(tutorial_category__category_slug = single_slug)
-        series_urls={}
+        matching_series = TutorialSeries.objects.filter(
+            tutorial_category__category_slug=single_slug)
+        series_urls = {}
         for m in matching_series.all():
-            part_one=Tutorial.objects.filter(tutorial_series__tutorial_series=m.tutorial_series).earliest("tutorial_published")
-            series_urls[m]=part_one.tutorial_slug
+            part_one = Tutorial.objects.filter(
+                tutorial_series__tutorial_series=m.tutorial_series).earliest("tutorial_published")
+            series_urls[m] = part_one.tutorial_slug
 
-        return render(request,"main/category.html",
-                    {"part_ones":series_urls})                
+        return render(request, "main/category.html",
+                      {"part_ones": series_urls})
 
     tutorials = [t.tutorial_slug for t in Tutorial.objects.all()]
-    if single_slug in tutorials :
-        this_tutorial= Tutorial.objects.get(tutorial_slug=single_slug)
-        return render(request, 
-        "main/tutorial.html",
-        {"tutorial":this_tutorial}
-        )
+    if single_slug in tutorials:
+        this_tutorial = Tutorial.objects.get(tutorial_slug=single_slug)
+        tutorial_from_series = Tutorial.objects.filter(
+            tutorial_series__tutorial_series=this_tutorial.tutorial_series).order_by("tutorial_published")
+        this_tutorial_idx = list(tutorial_from_series).index(this_tutorial)
+        return render(request,
+                      "main/tutorial.html",
+                      {"tutorial": this_tutorial,
+                       "sidebar": tutorial_from_series,
+                       "this_tutorial_idx": this_tutorial_idx}
+                      )
     return HttpResponse(f"{single_slug} does not correspond to  anything ")
 
+
 def homepage(request):
-    #return HttpResponse("Hello World")
-    return render(request=request,                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
-                template_name="main/categories.html",
-                context={"categories":TutorialCategory.objects.all})
+    # return HttpResponse("Hello World")
+    return render(request=request,
+                  template_name="main/categories.html",
+                  context={"categories": TutorialCategory.objects.all})
+
 
 def register(request):
-    if request.method== "POST":
-        form= UserCreationForm(request.POST)
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
         if form.is_valid():
-            user= form.save()
-            username=form.cleaned_data.get('username')
-            messages.success(request,f"New Account created {username}")
-            login(request,user)
-            messages.info(request,f"You are now logged  in {username}")
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"New Account created {username}")
+            login(request, user)
+            messages.info(request, f"You are now logged  in {username}")
             return redirect("main:homepage")
         else:
             for msg in form.error_messages:
-                messages.error(request,f"{msg}:{form.error_messages[msg]}")
+                messages.error(request, f"{msg}:{form.error_messages[msg]}")
 
-            return render(request = request,
-                          template_name = "main/register.html",
-                          context={"form":form})       
+            return render(request=request,
+                          template_name="main/register.html",
+                          context={"form": form})
 
-    form =UserCreationForm
+    form = UserCreationForm
     return render(request=request,
-                    template_name="main/register.html",
-                    context={"form":form})
+                  template_name="main/register.html",
+                  context={"form": form})
 
 
 def logout_request(request):
@@ -62,26 +72,24 @@ def logout_request(request):
     messages.info(request, "Logged out successfully")
     return redirect("main:homepage")
 
+
 def login_request(request):
     if request.method == "POST":
-        form= AuthenticationForm(request,data = request.POST)
+        form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            username=form.cleaned_data.get('username')
-            password=form.cleaned_data.get('password')
-            user=authenticate(username=username,password=password)
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
             if user is not None:
-                login(request,user)
-                messages.info(request,f"You are now logged  in {username}")
-                return  redirect("main:homepage")
+                login(request, user)
+                messages.info(request, f"You are now logged  in {username}")
+                return redirect("main:homepage")
             else:
-                messages.error(request,"Invalid username or  password")
+                messages.error(request, "Invalid username or  password")
         else:
-            messages.error(request,"Invalid username or  password")
+            messages.error(request, "Invalid username or  password")
 
-    form=AuthenticationForm()
+    form = AuthenticationForm()
     return render(request,
-                    "main/login.html",
-                    {"form":form})
-
-
-
+                  "main/login.html",
+                  {"form": form})
